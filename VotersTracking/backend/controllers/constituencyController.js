5db9b97c549b7160b8cd154662baa366bef92030
atta jnr <attajnr731@gmail.com>
2024-06-26 19:57:54 +0100

    constituency backend updated

--- VotersTracking/backend/controllers/constituencyController.js
+++ VotersTracking/backend/controllers/constituencyController.js
@@ -1,9 +1,76 @@
 import asyncHandler from "express-async-handler";
 import Constituency from "../models/Constituency.js";
 
+// @desc    Get all constituencies
+// @route   GET /api/constituencies
+// @access  Public
 const getConstituencies = asyncHandler(async (req, res) => {
-  const constituencies = await Constituency.find({}).select("name psCode");
+  const constituencies = await Constituency.find({});
   res.json(constituencies);
 });
 
-export { getConstituencies };
+// @desc    Create a new constituency
+// @route   POST /api/constituencies
+// @access  Private
+const createConstituency = asyncHandler(async (req, res) => {
+  const { name, psCode, coordinator, phone, population } = req.body;
+  const constituency = await Constituency.create({
+    name,
+    psCode,
+    coordinator,
+    phone,
+    population,
+  });
+
+  if (constituency) {
+    res.status(201).json(constituency);
+  } else {
+    res.status(400);
+    throw new Error("Invalid constituency data");
+  }
+});
+
+// @desc    Update a constituency
+// @route   PUT /api/constituencies/:id
+// @access  Private
+const updateConstituency = asyncHandler(async (req, res) => {
+  const { name, psCode, coordinator, phone, population } = req.body;
+
+  const constituency = await Constituency.findById(req.params.id);
+
+  if (constituency) {
+    constituency.name = name || constituency.name;
+    constituency.psCode = psCode || constituency.psCode;
+    constituency.coordinator = coordinator || constituency.coordinator;
+    constituency.phone = phone || constituency.phone;
+    constituency.population = population || constituency.population;
+
+    const updatedConstituency = await constituency.save();
+    res.json(updatedConstituency);
+  } else {
+    res.status(404);
+    throw new Error("Constituency not found");
+  }
+});
+
+// @desc    Delete a constituency
+// @route   DELETE /api/constituencies/:id
+// @access  Private
+const deleteConstituency = asyncHandler(async (req, res) => {
+  const constituency = await Constituency.findById(req.params.id);
+
+  if (constituency) {
+    await constituency.remove();
+    res.json({ message: "Constituency removed" });
+  } else {
+    res.status(404);
+    throw new Error("Constituency not found");
+  }
+});
+
+export {
+  getConstituencies,
+  createConstituency,
+  updateConstituency,
+  deleteConstituency,
+};
--- VotersTracking/backend/routes/constituencyRoutes.js
+++ VotersTracking/backend/routes/constituencyRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getConstituencies } from "../controllers/constituencyController.js";
+import {
+  getConstituencies,
+  createConstituency,
+  updateConstituency,
+  deleteConstituency,
+} from "../controllers/constituencyController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getConstituencies);
+router.route("/").get(getConstituencies).post(protect, createConstituency);
+router
+  .route("/:id")
+  .put(protect, updateConstituency)
+  .delete(protect, deleteConstituency);
 
 export default router;
